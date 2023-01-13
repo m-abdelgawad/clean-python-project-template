eafd19eb8569309cbef0178e9810c6436c4980f2
Mohamed AbdelGawad <muhammadabdelgawwad@gmail.com>
2023-01-13 11:23:48 +0200

    update logs path

--- python_project_template/__main__.py
+++ python_project_template/__main__.py
@@ -1,3 +1,4 @@
+import os
 import yaml
 from packages.project import absolute_path
 from packages.logger import logger
@@ -7,15 +8,17 @@ from packages.timestamp import timestamp
 def main():
 
     # Initiate logger
+    project_abs_path = os.path.dirname(absolute_path.get())
     logs_file_path = logger.create_log_file(
-        app_name='project-name', project_abs_path=absolute_path.get()
+        app_name='project-name', project_abs_path=project_abs_path
     )
     log = logger.setup_app_logger(logger_name='', log_file_path=logs_file_path)
 
     log.info('Start program execution')
 
     # Import configurations
-    with open('./conf.yaml') as config_file:
+    config_path = os.path.join(absolute_path.get(), 'conf.yaml')
+    with open(config_path) as config_file:
         conf = yaml.safe_load(config_file)
 
     # Start testing logger
