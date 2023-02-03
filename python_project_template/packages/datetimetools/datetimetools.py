df281c2308652187f8ca524aadf37757932d0dae
Mohamed AbdelGawad <muhammadabdelgawwad@gmail.com>
2023-02-03 15:27:05 +0200

    Added methods to the datetimetools package

--- python_project_template/packages/datetimetools/datetimetools.py
+++ python_project_template/packages/datetimetools/datetimetools.py
@@ -5,10 +5,10 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def get_current_timestamp():
-    log.info("Start datetimetools module")
-    current_timestamp = str(datetime.now().strftime("%Y-%m-%d__%H-%M-%S"))
-    log.info("Finished datetimetools module")
+def get_current_timestamp(output_format='%Y-%m-%d__%H-%M-%S'):
+    log.info("Start DateTimeTools module")
+    current_timestamp = str(datetime.now().strftime(output_format))
+    log.info("Finished DateTimeTools module")
     return current_timestamp
 
 
@@ -20,3 +20,11 @@ def get_past_date(days_count, output_format='%Y-%m-%d'):
     tday_date = datetime.now()
     past_date = timedelta(days=days_count)
     return (tday_date - past_date).strftime(output_format)
+
+
+def format_date(input_date, target_format):
+    return input_date.strftime(target_format)
+
+
+def format_str_date(input_date, current_format, target_format):
+    return datetime.strptime(input_date, current_format).strftime(target_format)
